55c5cb1ab15bb626be871b245f0e95c50c1f806d
honubin <honubin1@linefriends.com>
2023-07-19 17:32:59 +0900

    feat: Add problems

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	obj := Constructor(2)
-	obj.Put(2, 1)
-	obj.Put(1, 1)
-	obj.Put(2, 3)
-	obj.Put(4, 1)
-	fmt.Println(obj.Get(1))
-	fmt.Println(obj.Get(2))
+	// node_5 := ListNode{
+	// 	Val: 0,
+	// }
+	// node_4 := ListNode{
+	// 	Val:  4,
+	// 	Next: &node_5,
+	// }
+	node_3 := ListNode{
+		Val: 4,
+		// Next: &node_4,
+	}
+	node_2 := ListNode{
+		Val:  2,
+		Next: &node_3,
+	}
+	node_1 := ListNode{
+		Val:  3,
+		Next: &node_2,
+	}
+
+	fmt.Println(insertionSortList(&node_1))
 }
