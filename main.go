cd52fb211f30417df5fe14a138f850ad40646eb9
honubin <honubin1@linefriends.com>
2023-07-20 19:11:29 +0900

    feat: Add problems

--- main.go
+++ main.go
@@ -3,25 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	// node_5 := ListNode{
-	// 	Val: 0,
-	// }
-	// node_4 := ListNode{
-	// 	Val:  4,
-	// 	Next: &node_5,
-	// }
-	node_3 := ListNode{
-		Val: 4,
-		// Next: &node_4,
-	}
-	node_2 := ListNode{
-		Val:  2,
-		Next: &node_3,
-	}
-	node_1 := ListNode{
-		Val:  3,
-		Next: &node_2,
-	}
-
-	fmt.Println(insertionSortList(&node_1))
+	fmt.Println(asteroidCollision([]int{-2, -2, 2, -1}))
 }
