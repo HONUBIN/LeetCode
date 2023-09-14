fca6c2f563217fe3a3a6c9cda0efb203b97bc1d8
honubin <honubin1@linefriends.com>
2023-09-14 15:13:54 +0900

    feat: Add problems

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(findNumberOfLIS([]int{-100, -99, -98, -97, -96, -96}))
+	fmt.Println(findItinerary([][]string{{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}}))
 }
